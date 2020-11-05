f2887b8ab3e44631a0155273e880642cb0695373
AlanCristianMartinezGenaro <71827237+AlanCristianMartinezGenaro@users.noreply.github.com>
2020-11-05 07:40:14 -0600

    Add files via upload

---
+++ factura/Factura.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factura;
+
+/**
+ *
+ * @author marti
+ */
+public class Factura {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");   // TODO code application logic here
+    }
+}
